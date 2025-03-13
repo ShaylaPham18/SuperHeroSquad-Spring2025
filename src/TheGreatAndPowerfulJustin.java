6883495b9c3b37c79727135768eac46380a5403a
Justin <jmccabe1@ggc.edu>
2025-03-12 21:26:07 -0400

    Added another class with property and toStringed it Justin

---
+++ src/TheGreatAndPowerfulJustin.java
@@ -0,0 +1,19 @@
+public class TheGreatAndPowerfulJustin {
+    private String justin;
+    TheGreatAndPowerfulJustin(String justin){
+        this.justin=justin;
+    }
+
+    public String getJustin() {
+        return justin;
+    }
+
+    public void setJustin(String justin) {
+        this.justin = justin;
+    }
+
+    @Override
+    public String toString() {
+        return justin;
+    }
+}
