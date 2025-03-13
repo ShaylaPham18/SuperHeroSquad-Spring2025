182a2f920588906c4ab05250c608350c172571b6
KrazyKlakes <themysteriosguy53@gmail.com>
2025-03-13 19:39:07 -0400

    Added SuperHeroSquad class with greeting method - Jose Montejo

---
+++ src/SuperHeroSquad.java
@@ -0,0 +1,19 @@
+/**
+ * This class represents a superhero squad with various abilities.
+ * It includes methods to manage and display superhero information.
+ */
+public class SuperHeroSquad {
+
+    // Method to display a greeting message
+    public void displayGreeting() {
+        System.out.println("Welcome to the SuperHero Squad!");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Create an instance of SuperHeroSquad
+        SuperHeroSquad squad = new SuperHeroSquad();
+        // Call the displayGreeting method
+        squad.displayGreeting();
+    }
+}
