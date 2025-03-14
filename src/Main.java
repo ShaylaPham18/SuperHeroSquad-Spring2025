41c1121fa4f24e527d5e7b0d3df27393e6062d25
Nelly Eligwe <neligwe@ggc.edu>
2025-03-13 21:44:26 -0400

    configured project and fixed intellij setup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Razan");
-        TheGreatAndPowerfulJustin theGreatAndPowerfulJustin=new TheGreatAndPowerfulJustin("Justin");
+
+        // Correctly declare and initialize the object
+        TheGreatAndPowerfulJustin theGreatAndPowerfulJustin = new TheGreatAndPowerfulJustin("I am The Great And Powerful Justin!");
+
+        // Print the object
         System.out.println(theGreatAndPowerfulJustin);
     }
 }
